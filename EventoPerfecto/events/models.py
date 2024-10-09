fae4a7baa5e6bfb6ffafba3f969fc9e7df6c9373
NiharikaTripathi <ntripathi612@gmail.com>
2024-10-09 23:42:59 +0530

    Initial Commit

---
+++ EventoPerfecto/events/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Event models
+class Venue(models.Model):
+    name = models.CharField('Event Name', max_length=120)
+    address = models.CharField(max_length=300)
+    zip_code = models.CharField('Zip Code', max_length=15)
+    phone = models.CharField('Phone Number', max_length=25)
+    web = models.URLField('Website Address')
+    email = models.EmailField('Email')
+
+    def __str__(self):
+        return self.name
+
+# Event models
+class MyClubUser(models.Model):
+    first_name = models.CharField('First Name', max_length=120)
+    last_name = models.CharField('Last Name', max_length=120)
+    email = models.EmailField('User Email')
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+# Event models
+class Event(models.Model):
+    name = models.CharField('Event Name', max_length=120)
+    event_date = models.DateTimeField('Event Date')
+    venue = models.ForeignKey(Venue, blank=True, null=True)
+    # venue = models.CharField('Venue', max_length=120)
+    manager = models.CharField('Manager Name', max_length=120)
+    description = models.TextField('Event Description', max_length=120)
+    attendees = models.ManyToManyField(MyClubUser, blank=True)
+
+    def __str__(self):
+        return self.name
